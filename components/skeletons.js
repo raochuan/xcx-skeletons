d04d863ddb06821b68c618788850d6594852f5ae
rc13193 <rc13193@ly.com>
2018-12-19 11:05:25 +0800

    获取DOM 并计算

--- components/skeletons.js
+++ components/skeletons.js
@@ -4,7 +4,17 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    nodes_complete:{
+      type:Boolean,
+      default:false,
+      observer(newVal,oldVal){
+        if (newVal){
+          //完成nodes节点渲染
+          let selector = this.getSelector(".skeletons-circle")
+          this.getQueryBoundRect(selector);
+        }
+      }
+    }
   },
 
   /**
@@ -13,11 +23,20 @@ Component({
   data: {
 
   },
-
+  attached(){
+    
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    getSelector:(name) => {
+      return wx.createSelectorQuery().selectAll(name)
+    },
+    getQueryBoundRect:(selector) => {
+      selector.boundingClientRect().exec((res) => {
+        console.log ("dom选择后:",res)
+      })
+    }
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    nodesComplete:false
   },
 
   /**
@@ -19,14 +19,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    this.setData({
+      nodesComplete:true
+    })
   },
-
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log("ready")
   },
 
   /**
